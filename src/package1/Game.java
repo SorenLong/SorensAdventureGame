1932025c7229eb5ebc1c07ab5ff8642b1b1d3bc5
soren <sorenxlong@gmail.com>
2022-10-26 17:16:55 -0400

    :-)

--- src/package1/Game.java
+++ src/package1/Game.java
@@ -1,5 +1,8 @@
 package package1;
+
+
 //Ur at like 13:43 in the tutoiral
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -10,16 +13,40 @@ public class Game {
     VisibilityManager vm = new VisibilityManager(ui);
 
     public static void main(String[] args){
+
         new Game();
+
     }
 
     public Game(){
+
         ui.createUI(cHandler);
         vm.showTitleScreen();
+
     }
 
     public class ChoiceHandler implements ActionListener{
+
         public void actionPerformed(ActionEvent event){
+
+            String yourChoice = event.getActionCommand();
+
+            switch(yourChoice){
+                case "start": break;
+                case "c1": break;
+                case "c2": break;
+                case "c3": break;
+                case "c4": break;
+
+
+
+
+            }
+
+
         }
+
     }
+
+
 }
--- src/package1/VisibilityManager.java
+++ src/package1/VisibilityManager.java
@@ -3,19 +3,37 @@ package package1;
 public class VisibilityManager {
 
     UI ui;
+
     public VisibilityManager(UI userInterface){
         ui = userInterface;
-
     }
 
-    public void ShowTitleScreen(){
-        //show title screen
+    public void showTitleScreen(){
+        //SHOW TITLE SCREEN
         ui.titleNamePanel.setVisible(true);
         ui.startButtonPanel.setVisible(true);
 
-        //Hide the game screen
+        //HIDE THE GAME SCREEN
         ui.maintextPanel.setVisible(false);
         ui.choiceButtonPanel.setVisible(false);
         ui.playerPanel.setVisible(false);
+
+
     }
+
+    public void titleToTown(){
+
+        //Hide the title screen
+        ui.titleNamePanel.setVisible(false);
+        ui.startButtonPanel.setVisible(false);
+
+        //SHOW THE GAME SCREEN
+        ui.maintextPanel.setVisible(true);
+        ui.choiceButtonPanel.setVisible(true);
+        ui.playerPanel.setVisible(true);
+
+    }
+
+
+
 }
